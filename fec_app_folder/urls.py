43bfcd891419e07465922b9a0d94486ef832cfaf
Koki-route-free <ykouki0112@gmail.com>
2022-08-16 23:25:45 +0900

    page add

--- fec_app_folder/urls.py
+++ fec_app_folder/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'fec_app_folder'
 urlpatterns = [
-    path('top_page/', views.top_page, name='top_page')
+    path('admins/login/', views.admins_login, name='admins_login'),
+    path('admins/solid/', views.admins_solid, name='admins_solid'),
+    path('admins/temporary/', views.admins_temporary, name='admins_temporary'),
+    path('users/top_page/', views.users_top_page, name='users_top_page'),
+    path('users/reviews/', views.users_reviews, name='users_reviews'),
 ]
--- fec_app_folder/views.py
+++ fec_app_folder/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.views import View
 
-class SampleView(View):  
+class Admins_Solid_View(View):  
     def get(self, request, *args, **kwargs):  
-        return render(request, 'contents/top_page.html')
-      
-      
+        return render(request, 'admins/solid.html')
 
-top_page = SampleView.as_view()
+
+class Admins_Temporary_View(View):  
+    def get(self, request, *args, **kwargs):  
+        return render(request, 'admins/temporary.html')
+
+
+class Admins_Login_View(View):  
+    def get(self, request, *args, **kwargs):  
+        return render(request, 'admins/login.html')
+
+
+class Users_Reviews_View(View):  
+    def get(self, request, *args, **kwargs):  
+        return render(request, 'users/reviews.html')
+
+    
+class Users_Top_page_View(View):  
+    def get(self, request, *args, **kwargs):  
+        return render(request, 'users/top_page.html')
+
+
+admins_solid = Admins_Solid_View.as_view()
+admins_temporary = Admins_Temporary_View.as_view()
+admins_login = Admins_Login_View.as_view()
+users_reviews = Users_Reviews_View.as_view()
+users_top_page = Users_Top_page_View.as_view()
