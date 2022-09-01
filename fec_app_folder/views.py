4b27e201ef53ac0c6f7e83787c2a749cd9546901
Kamoda_Yohei <kammmmowmaf88@gmail.com>
2022-09-02 04:10:22 +0900

    Yohei commit 0902

--- fec_app_folder/views.py
+++ fec_app_folder/views.py
@@ -38,7 +38,11 @@ class Users_Reviews_View(View):
     
 class Users_Top_page_View(View):  
     def get(self, request, *args, **kwargs):  
-        return render(request, 'users/top_page.html')
+        context = {'F3classrooms': ["F301", "F302", "F303", "F304", "F305", "F306", "F307", "F308", "F309", "F310", "F311", "F312"],
+                   'F4classrooms': ["F401", "F402", "F403", "F404", "F405", "F406", "F407", "F408", "F409", "F410", "F411"],
+                   'F5classrooms': ["F501", "F502", "F503", "F504", "F505", "F506", "F507", "F508", "F509", "F510", "F511", "F512", "F513"],
+                   'F6classrooms': ["F601", "F602", "F603", "F604", "F605", "F606", "F607", "F608"],}
+        return render(request, 'users/top_page.html', context)
 
 
 admins_solid = Admins_Solid_View.as_view()
